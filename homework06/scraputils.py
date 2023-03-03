07b60ea0b03dda95db6e8fc9ce2d854e0baaf04e
nikolayignatenkov@MacBook-Pro-Nikolay <randomezer@mail.ru>
2023-03-03 16:47:46 +0300

    s

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,3 +1,6 @@
+# fmt: off
+# mypy: ignore-errors
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,15 +8,28 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    m = {
+        'author': parser.select('.hnuser'),
+        'comments': parser.select('.subline > a:last-child'),
+        'points': parser.select('span.score'),
+        'title': parser.select('span.titleline > a'),
+        'url': parser.select('span.titleline > a')
+    }
+    for i in range(30):
+        comments = m['comments'][i].text.split()
+        news_list.append({
+            'author': m['author'][i].text,
+            'comments': int(comments[0]) if len(comments) == 2 else 0,
+            'points': int(m['points'][i].text.split()[0]),
+            'title': m['title'][i].text,
+            'url': m['url'][i]['href'],
+        })
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    return parser.select_one('.morelink')['href']
 
 
 def get_news(url, n_pages=1):
@@ -29,4 +45,3 @@ def get_news(url, n_pages=1):
         news.extend(news_list)
         n_pages -= 1
     return news
-
